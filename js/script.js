417a2f22f96d257522706f7cd1afb0d239dff269
elenagolovko <airdragon220@gmail.com>
2017-12-05 22:50:27 +0300

    анимация, доступность, интерактивная карта

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+var link = document.querySelector(".overlay-link");
+var popup = document.querySelector(".write-to-us-container");
+var modal = popup.querySelector(".write-to-us");
+var close = popup.querySelector(".modal-close");
+var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var comment = popup.querySelector("[name=comment]");
+var form = popup.querySelector("form");
+
+link.addEventListener("click", function (evt) {
+  popup.classList.add("overlay-show");
+  login.focus();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("overlay-show");
+  modal.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+if (!login.value || !email.value || !comment.value) {
+  evt.preventDefault();
+  modal.classList.add("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("overlay-show")) {
+      popup.classList.remove("overlay-show");
+    }
+  }
+});
